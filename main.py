5dac5bc243418bd91fb0eb1da1db219710f68ece
Sam Branka <sdbranka@me.com>
2021-09-14 10:20:28 -0400

    Player physics, jump added; floor now barrier

--- main.py
+++ main.py
@@ -32,11 +32,15 @@ sky_surface = pygame.image.load('img/Sky.png').convert()
 # create a ground surface with an image
 ground_surface = pygame.image.load('img/ground.png').convert()
 
-# create text surface
-text_surface_text = "My Game"
-text_surface_anti_aliasing = False
-text_surface_color = "Black"
-text_surface = test_font.render(text_surface_text, text_surface_anti_aliasing, text_surface_color)
+# create score surface
+score_surface_text = "My Game"
+score_surface_anti_aliasing = False
+# score_surface_color = "Black"
+score_surface_color = (64, 64, 64)
+score_surface = test_font.render(score_surface_text, score_surface_anti_aliasing, score_surface_color)
+score_pos_x = 400
+score_pos_y = 27
+score_rect = score_surface.get_rect(midtop = (score_pos_x, score_pos_y))
 
 # create snail character(surface)
 snail_surface = pygame.image.load("img/snail/snail1.png").convert_alpha()
@@ -45,14 +49,24 @@ snail_pos_y = 300
 # create snail rectangle that is same size as snail_surface
 snail_rect = snail_surface.get_rect(midbottom = (snail_pos_x, snail_pos_y))
 
-
-
 # create player character(surface)
 player_surface = pygame.image.load("img/player/player_walk_1.png").convert_alpha()
 player_pos_x = 80
 player_pos_y = 300
 # create player rectangle that is same size as player_surface
 player_rect = player_surface.get_rect(midbottom = (player_pos_x, player_pos_y))
+# create gravity physics for player
+player_gravity = 0
+
+
+
+
+
+
+
+
+
+
 
 # game loop
 while True:
@@ -61,6 +75,40 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # if the mouse moves the terminal will output the mouse position 
+        # if event.type == pygame.MOUSEMOTION:
+        #     print(event.pos)
+        # if mouse button is depressed print "mouse down"
+        # if event.type == pygame.MOUSEBUTTONDOWN:
+        #     print("mouse down")
+        # if mouse button is not depressed print "mouse up"
+        # if event.type == pygame.MOUSEBUTTONUP:
+        #     print("mouse up")
+        # check to see if mouse is over player_rect
+        # if event.type == pygame.MOUSEMOTION:
+            # print(event.pos)
+            # if player_rect.collidepoint(event.pos):
+            #     print("collision")
+        # learning KEYUP and KEYDOWN
+        # if event.type == pygame.KEYDOWN:
+        #     print("Key down")
+        # if event.type == pygame.KEYUP:
+        #     print("Key up")
+        # check for a specific key press
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_SPACE:
+        #         print("jump")
+        
+        # make player jump using space bar
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player_gravity = -20
+
+        # make player jump if player is clicked (1:43:15)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rect.collidepoint(event.pos):
+                player_gravity = -20
+
 
     # display test_surface
     # ts_pos_x = 200
@@ -77,19 +125,74 @@ while True:
     ground_pos_y = 300
     screen.blit(ground_surface, (ground_pos_x, ground_pos_y))
 
-    # display text_surface
-    text_pos_x = 300
-    text_pos_y = 50
-    screen.blit(text_surface, (text_pos_x, text_pos_y))
+    # adding a background to the score_surface
+    surface_to_draw_score_on = screen
+    # score_bg_color = "Pink"
+    score_bg_color = "#c0e8ec"
+    rect_to_draw = score_rect
+    pygame.draw.rect(surface_to_draw_score_on, score_bg_color, rect_to_draw)
+    pygame.draw.rect(surface_to_draw_score_on, score_bg_color, rect_to_draw, 10)
+
+    # display score_surface
+    screen.blit(score_surface, score_rect)
 
     # display snail/move position
     snail_rect.left -= 3
-    if snail_rect.left < -90:
+    if snail_rect.right < 0:
         snail_rect.left = 800
     screen.blit(snail_surface, snail_rect)
 
+    # apply player gravity physics
+    player_gravity +=1 
+    player_rect.y += player_gravity
+    # create floor as barrier
+    if player_rect.bottom >= 300:
+        player_rect.bottom = 300
+
     # display player
-    screen.blit(player_surface, player_rect )
+    screen.blit(player_surface, player_rect)
+
+    # stores pressed keys into variable like a dictionary, if space bar is pushed print jump
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_SPACE]:
+    #     print("jump")
+
+
+    # check if player_rect() has collided with snail_rect
+    # player_rect.colliderect(snail_rect) generates a number value either 1 or 0 which may be used as a Boolean check
+    # print(player_rect.colliderect(snail_rect))
+    # if player_rect.colliderect(snail_rect):
+        # print("collision")
+
+    # determines mouse position, if mouse collides with player_rect the terminal will output which buttons on the mouse are being pressed
+    # mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint(mouse_pos):
+        # print(pygame.mouse.get_pressed())
+
+    # draw a line from the top left corner to the bottom right corner
+    # surface_to_draw_line_on = screen
+    # line_color = "Gold"
+    # line_start_point = (0,0)
+    # line_end_point = (800, 400)
+    # line_width = 10
+    # pygame.draw.line(surface_to_draw_line_on, line_color, line_start_point, line_end_point, line_width)
+
+    # draw a line from top left corner to the mouse position
+    # surface_to_draw_line_on = screen
+    # line_color = "Gold"
+    # line_start_point = (0,0)
+    # line_end_point = pygame.mouse.get_pos()
+    # line_width = 10
+    # pygame.draw.line(surface_to_draw_line_on, line_color, line_start_point, line_end_point, line_width)
+
+    # draw ellipse while designing our own rect
+    # surface_to_ellipse_line_on = screen
+    # ellipse_color = "Brown"
+    # ellipse_x = 50
+    # ellipse_y = 200
+    # ellipse_width = 100
+    # ellipse_height = 100
+    # pygame.draw.ellipse(surface_to_ellipse_line_on, ellipse_color, pygame.Rect(ellipse_x, ellipse_y, ellipse_width, ellipse_height))
 
 
     pygame.display.update()
