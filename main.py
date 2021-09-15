50e63cc06aeffb9734e4fd879185550ec34af99d
Sam Branka <sdbranka@me.com>
2021-09-14 20:46:45 -0400

    game over/intro screen built

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import pygame
 from sys import exit
+import math
+
+
+# function to consistantly update and redraw score_surface
+def display_score():
+    current_time = (pygame.time.get_ticks() - start_time) / 1000
+    score_surface = test_font.render(f"Score: {math.floor(current_time)}", False, (64, 64, 64))
+    score_rect = score_surface.get_rect(midtop = (400, 27))
+    screen.blit(score_surface, score_rect)
+    return current_time
+
+
+
+
 
 # initialize pygame
 pygame.init()
@@ -21,7 +35,15 @@ font_size = 50
 test_font = pygame.font.Font(font_type, font_size)
 
 # Setting up game states
-game_active = True
+    # setting to false starts game from game over screen
+game_active = False
+
+
+# building to be able to reset the score/timer count to zero with new game
+start_time = 0
+
+# store the score for global use
+score = 0
 
 
 
@@ -39,14 +61,14 @@ sky_surface = pygame.image.load('img/Sky.png').convert()
 ground_surface = pygame.image.load('img/ground.png').convert()
 
 # create score surface
-score_surface_text = "My Game"
-score_surface_anti_aliasing = False
-# score_surface_color = "Black"
-score_surface_color = (64, 64, 64)
-score_surface = test_font.render(score_surface_text, score_surface_anti_aliasing, score_surface_color)
-score_pos_x = 400
-score_pos_y = 27
-score_rect = score_surface.get_rect(midtop = (score_pos_x, score_pos_y))
+# score_surface_text = "My Game"
+# score_surface_anti_aliasing = False
+# # score_surface_color = "Black"
+# score_surface_color = (64, 64, 64)
+# score_surface = test_font.render(score_surface_text, score_surface_anti_aliasing, score_surface_color)
+# score_pos_x = 400
+# score_pos_y = 27
+# score_rect = score_surface.get_rect(midtop = (score_pos_x, score_pos_y))
 
 # create snail character(surface)
 snail_surface = pygame.image.load("img/snail/snail1.png").convert_alpha()
@@ -63,11 +85,28 @@ player_pos_y = 300
 player_rect = player_surface.get_rect(midbottom = (player_pos_x, player_pos_y))
 # create gravity physics for player
 player_gravity = 0
+# creating player surface for game over/intro screen
+player_stand = pygame.image.load("img/player/player_stand.png").convert_alpha()
+pss_surface_to_use = player_stand
+# # scale by dimensions
+# pss_scale_to_width = 200
+# pss_scale_to_height = 400
+# player_stand = pygame.transform.scale(pss_surface_to_use, (pss_scale_to_width, pss_scale_to_height))
+# # scale image to double it's size
+# player_stand = pygame.transform.scale2x(pss_surface_to_use)
+# scale using rotozoom (it rotates a surface, scales a surface, it filters a surface)
+pss_rotation_angle = 0
+pss_scale = 2
+player_stand = pygame.transform.rotozoom(pss_surface_to_use, pss_rotation_angle, pss_scale)
+player_stand_rect = player_stand.get_rect(center = (400, 200))
 
 
+# game name
+game_name = test_font.render("Pixel Runner", False, (111, 196, 169))
+game_name_rect = game_name.get_rect(center = (400, 80))
 
-
-
+game_msg = test_font.render("Press space to run", False, (111, 196, 169))
+game_msg_rect = game_msg.get_rect(center = (400, 330))
 
 
 
@@ -116,9 +155,15 @@ while True:
                 if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
                     player_gravity = -20
         else:
+            # if user clicks the window close button
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            # user presses space bar to restart game
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                     game_active = True
                     snail_rect.left = 800
+                    start_time = pygame.time.get_ticks()
 
 
     if game_active:
@@ -137,16 +182,28 @@ while True:
         ground_pos_y = 300
         screen.blit(ground_surface, (ground_pos_x, ground_pos_y))
 
-        # adding a background to the score_surface
-        surface_to_draw_score_on = screen
-        # score_bg_color = "Pink"
-        score_bg_color = "#c0e8ec"
-        rect_to_draw = score_rect
-        pygame.draw.rect(surface_to_draw_score_on, score_bg_color, rect_to_draw)
-        pygame.draw.rect(surface_to_draw_score_on, score_bg_color, rect_to_draw, 10)
+        # # adding a background to the score_surface
+        # surface_to_draw_score_on = screen
+        # # score_bg_color = "Pink"
+        # score_bg_color = "#c0e8ec"
+        # rect_to_draw = score_rect
+        # pygame.draw.rect(surface_to_draw_score_on, score_bg_color, rect_to_draw)
+        # pygame.draw.rect(surface_to_draw_score_on, score_bg_color, rect_to_draw, 10)
+        # # display score_surface
+        # screen.blit(score_surface, score_rect)
 
-        # display score_surface
-        screen.blit(score_surface, score_rect)
+        # # display score_surface using a function
+        # display_score()
+        # store score and display
+        score = display_score()
+
+
+
+
+
+
+
+
 
         # display snail/move position
         snail_rect.left -= 3
@@ -210,8 +267,16 @@ while True:
         if snail_rect.colliderect(player_rect):
             game_active = False
     else:
-        screen.fill("Yellow")
+        screen.fill((94, 129, 162))
+        screen.blit(player_stand, player_stand_rect)
 
+        score_msg = test_font.render(f"Your score: {math.floor(score)}", False, (111, 196, 169))
+        score_msg_rect = score_msg.get_rect(center = (400, 330))
+        screen.blit(game_name, game_name_rect)
+        if score == 0:
+            screen.blit(game_msg, game_msg_rect)
+        else:
+            screen.blit(score_msg, score_msg_rect)
 
     pygame.display.update()
     # sets so that while True loop should not run faster than framerate_ceiling times per second
